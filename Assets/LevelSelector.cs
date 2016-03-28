e0fd4a2491ee9801cf6a4621c2ec34a38278c35d
Eric <blazingspike.liu@gmail.com>
2016-03-28 09:21:29 -0700

    stuff

--- Assets/LevelSelector.cs
+++ Assets/LevelSelector.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using Gamelogic.Grids;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class LevelSelector : MonoBehaviour {
 
@@ -9,12 +10,19 @@ public class LevelSelector : MonoBehaviour {
 
   private IMap<RectPoint> map;
 
+  private Canvas modeChanger;
 	// Use this for initialization
 	void Start () {
+    modeChanger = GameObject.Find ("ModeChanger").GetComponent<Canvas> ();
+    modeChanger.enabled = false;
+    setMode (LevelUtils.getCurrentMode ());
     BuildLevelGrid ();
 	}
 
   void Update() {
+    if (modeChanger.enabled) {
+      return;
+    }
     if (Input.GetMouseButtonDown (0)) {
       Vector3 worldPosition = GridBuilderUtils.ScreenToWorld (gameObject, Input.mousePosition);
       RectPoint point = map [worldPosition];
@@ -51,5 +59,52 @@ public class LevelSelector : MonoBehaviour {
       grid [point] = cell;
       count += 1;
     }
+  }
+
+  public void onModeChangeClicked() {
+    modeChanger.enabled = true;
+    int maxMode = LevelUtils.getMaxMode ();
+    if (maxMode < 4) {
+      GameObject.Find ("InsaneBtn").GetComponent<Button> ().interactable = false;
+    } else {
+      GameObject.Find ("InsaneBtn").GetComponent<Button> ().interactable = true;
+    }
+    if (maxMode < 3) {
+      GameObject.Find ("VeryHardBtn").GetComponent<Button> ().interactable = false;
+    } else {
+      GameObject.Find ("VeryHardBtn").GetComponent<Button> ().interactable = true;
+    }
+    if (maxMode < 2) {
+      GameObject.Find ("HardBtn").GetComponent<Button> ().interactable = false;
+    } else {
+      GameObject.Find ("HardBtn").GetComponent<Button> ().interactable = true;
+    }
+  }
+
+  private void setMode(int m) {
+    LevelUtils.setCurrentMode (m);
+    modeChanger.enabled = false;
+    Button modeBtn = GameObject.Find ("ModeBtn").GetComponent<Button> ();
+    modeBtn.GetComponentInChildren<Text> ().text = LevelUtils.getModeString (m);
+  }
+
+  public void onEasyClicked() {
+    setMode (0);
+  }
+
+  public void onNormalClicked() {
+    setMode (1);
+  }
+
+  public void onHardClicked() {
+    setMode (2);
+  }
+
+  public void onVeryHardClicked() {
+    setMode (3);
+  }
+
+  public void onInsaneClicked() {
+    setMode (4);
   }
 }
